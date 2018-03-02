a183b58506a49e2e8614ca89adea195d10c8d12d
Sergey Solomentsev <serg.solomentsev@ya.ru>
2018-03-02 14:02:27 +0200

    Introduced type extensions for internal use.

---
+++ Pipelines/ExtensionMethods/ObjectExtensionMethods.cs
@@ -0,0 +1,30 @@
+﻿namespace Pipelines.ExtensionMethods
+{
+    internal static class ObjectExtensionMethods
+    {
+        public static bool IsNull<T>(this T obj) where T : class
+        {
+            return obj == null;
+        }
+
+        public static bool IsNotNull<T>(this T obj) where T : class
+        {
+            return !IsNull(obj);
+        }
+
+        public static bool HasValue<T>(this T obj) where T : class
+        {
+            return IsNotNull(obj);
+        }
+
+        public static bool HasNoValue<T>(this T obj) where T : class
+        {
+            return !HasValue(obj);
+        }
+
+        public static T Ensure<T>(this T obj, T value) where T : class
+        {
+            return obj.HasValue() ? obj : value;
+        }
+    }
+}
