10a7e0a6678aa7f62b15b4b352e12b372f96028f
Sergey Solomentsev <serg.solomentsev@ya.ru>
2018-03-05 17:03:26 +0200

    Added couple simple integraion tests, producing "hello world" result.

---
+++ Pipelines.Tests.Integrations/HelloWorldIntegrationTest.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using FluentAssertions;
+using Xunit;
+
+namespace Pipelines.Tests.Integrations
+{
+    public class HelloWorldIntegrationTest
+    {
+        [Fact]
+        public async void Pipeline_Mechanism_Is_Able_To_Produce_A_Simple_Result()
+        {
+            PipelineRunner runner = new PipelineRunner();
+
+            var arguments = new HelloWorldArguments() { Name = "Sergey" };
+            await runner.RunPipeline(new HelloWorldPipeline(), arguments);
+
+            arguments.Result.Should().Be("Hello, Sergey!",
+                $"we've passed name '{arguments.Name}' to the pipeline, and expect it to be displayed in phrase 'Hello, {arguments.Name}!'");
+        }
+
+        [Fact]
+        public async void Pipeline_Mechanism_Produces_A_Simple_Check_Before_Returning_A_Result()
+        {
+            PipelineRunner runner = new PipelineRunner();
+
+            var arguments = new HelloWorldArguments { Name = "   " };
+            await runner.RunPipeline(new HelloWorldPipelineWithValidation(), arguments);
+
+            arguments.GetMessages(MessageFilter.Errors).Should().ContainSingle(pipelineMessage =>
+                pipelineMessage.Message.Equals(HelloWorldPipelineMessages.NameMustBeProvided));
+        }
+    }
+
+    public class HelloWorldPipeline : IPipeline
+    {
+        public IEnumerable<IProcessor> GetProcessors()
+        {
+            return new HelloWorldProcessors[]
+            {
+                new PutNameIntoThePhrase()
+            };
+        }
+    }
+
+    public class HelloWorldPipelineWithValidation : IPipeline
+    {
+        public IEnumerable<IProcessor> GetProcessors()
+        {
+            return new HelloWorldProcessors[]
+            {
+                new WhenTheNameIsNotProvidedAbortWithErrorMessage(),
+                new PutNameIntoThePhrase()
+            };
+        }
+    }
+
+    public static class HelloWorldPipelineMessages
+    {
+        public static readonly string NameMustBeProvided =
+            "To get a proper result of this pipeline, please provide a valuable name.";
+    }
+
+    public abstract class HelloWorldProcessors : SafeProcessor<HelloWorldArguments> { }
+
+    public class WhenTheNameIsNotProvidedAbortWithErrorMessage : HelloWorldProcessors
+    {
+        public override Task SafeExecute(HelloWorldArguments args)
+        {
+            args.AbortPipelineWithErrorMessage(HelloWorldPipelineMessages.NameMustBeProvided);
+            return Task.CompletedTask;
+        }
+
+        public override bool SafeCondition(HelloWorldArguments args)
+        {
+            return base.SafeCondition(args) && string.IsNullOrWhiteSpace(args.Name);
+        }
+    }
+
+    public class PutNameIntoThePhrase : HelloWorldProcessors
+    {
+        public override Task SafeExecute(HelloWorldArguments args)
+        {
+            args.Result = "Hello, " + args.Name + "!";
+            return Task.CompletedTask;
+        }
+    }
+
+    public class HelloWorldArguments : PipelineContext
+    {
+        public string Name { get; set; }
+        public string Result { get; set; }
+    }
+}
