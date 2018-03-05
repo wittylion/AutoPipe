7a5605390f88f7a0e12838754877d5a069be7f3b
Sergey Solomentsev <serg.solomentsev@ya.ru>
2018-03-05 16:10:19 +0200

    Added integration test project ot the solution.

---
+++ Pipelines.Tests.Integrations/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Pipelines.Tests.Integrations")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Pipelines.Tests.Integrations")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("faa81ef4-69dc-45a8-a990-2d78c08c951d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
