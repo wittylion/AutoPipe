403c4b133c23ebc673f1c27f33713326ae7f9970
Sergey Solomentsev <serg.solomentsev@ya.ru>
2018-03-06 15:59:23 +0200

    Run pipeline method is divided into more concrete methods.

--- Pipelines/PipelineRunner.cs
+++ Pipelines/PipelineRunner.cs
@@ -1,4 +1,5 @@
-﻿using System.Linq;
+﻿using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using Pipelines.ExtensionMethods;
 
@@ -6,18 +7,29 @@ namespace Pipelines
 {
     public class PipelineRunner
     {
-        public virtual async Task RunPipeline<TArgs>(IPipeline pipeline, TArgs args)
+        public virtual Task RunPipeline<TArgs>(IPipeline pipeline, TArgs args)
         {
             if (pipeline.HasNoValue())
             {
-                return;
+                return Task.CompletedTask;
             }
 
-            var processors = pipeline.GetProcessors().Ensure(Enumerable.Empty<IProcessor>());
+            return RunProcessors(pipeline.GetProcessors(), args);
+        }
+
+        public virtual async Task RunProcessors<TArgs>(IEnumerable<IProcessor> processors, TArgs args)
+        {
+            processors = processors.Ensure(Enumerable.Empty<IProcessor>());
             foreach (var processor in processors)
             {
-                await processor.Execute(args);
+                await RunProcessor(processor, args);
             }
+        }
+
+        public virtual async Task RunProcessor<TArgs>(IProcessor processor, TArgs args)
+        {
+            if (processor.HasValue())
+                await processor.Execute(args);
         }
     }
 }
