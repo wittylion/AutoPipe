20cb385c1e9a924a5bf53518e60832f259e21f1a
Sergey Solomentsev <serg.solomentsev@ya.ru>
2018-12-07 19:31:26 +0200

    Added extensions for pipeline context that execute action in case property exists or doesn't exist.

---
+++ Pipelines/ExtensionMethods/PipelineContextExtensionMethods.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Pipelines.ExtensionMethods
+{
+    /// <summary>
+    /// Extensions for the <see cref="PipelineContext"/>.
+    /// </summary>
+    public static class PipelineContextExtensionMethods
+    {
+        /// <summary>
+        /// Executes an action for pipeline context in case there is a property
+        /// passed by <paramref name="property"/> exists.
+        /// </summary>
+        /// <typeparam name="TContext">Type of the pipeline context that is extended.</typeparam>
+        /// <param name="context">
+        /// Pipeline context that is extended.
+        /// </param>
+        /// <param name="property">
+        /// The property to be checked before executing action.
+        /// </param>
+        /// <param name="action">
+        /// The action to be executed when the property exists.
+        /// </param>
+        public static void IfHasProperty<TContext>(this TContext context, string property, Action<TContext> action)
+            where TContext : PipelineContext
+        {
+            if (context.HasProperty(property))
+            {
+                action(context);
+            }
+        }
+
+        /// <summary>
+        /// Executes an action for pipeline context in case there is no property
+        /// passed by <paramref name="property"/> exists.
+        /// </summary>
+        /// <typeparam name="TContext">Type of the pipeline context that is extended.</typeparam>
+        /// <param name="context">
+        /// Pipeline context that is extended.
+        /// </param>
+        /// <param name="property">
+        /// The property to be checked before executing action.
+        /// </param>
+        /// <param name="action">
+        /// The action to be executed when the property does not exist.
+        /// </param>
+        public static void IfHasNoProperty<TContext>(this TContext context, string property, Action<TContext> action)
+            where TContext : PipelineContext
+        {
+            if (!context.HasProperty(property))
+            {
+                action(context);
+            }
+        }
+    }
+}
