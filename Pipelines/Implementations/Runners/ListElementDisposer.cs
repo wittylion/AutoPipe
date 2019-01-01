49123b5d0e8b7544b84ee39726fe9427cd81fccc
Sergey Solomentsev <serg.solomentsev@ya.ru>
2019-01-01 22:11:45 +0200

    Added documentation for observable pipeline and processor runners.

--- Pipelines/Implementations/Runners/ListElementDisposer.cs
+++ Pipelines/Implementations/Runners/ListElementDisposer.cs
@@ -3,17 +3,44 @@ using System.Collections.Generic;
 
 namespace Pipelines.Implementations.Runners
 {
+    /// <summary>
+    /// An implementation of <see cref="IDisposable"/>
+    /// that removes an item from a list.
+    /// </summary>
+    /// <typeparam name="TItem">
+    /// The type of the item that has to be removed from the collection.
+    /// </typeparam>
     internal class ListElementDisposer<TItem> : IDisposable
     {
+        /// <summary>
+        /// The list from which an element should be deleted.
+        /// </summary>
         private List<TItem> List { get; }
+
+        /// <summary>
+        /// The item to be deleted from the list.
+        /// </summary>
         private TItem Item { get; }
 
+        /// <summary>
+        /// Constructor that takes list from which
+        /// an item should be deleted, and item
+        /// that has to be removed from the collection.
+        /// </summary>
+        /// <param name="list">
+        /// List to be used when an item removed.
+        /// </param>
+        /// <param name="item">
+        /// An item to be removed from the list when <see cref="Dispose"/>
+        /// method is called.
+        /// </param>
         public ListElementDisposer(List<TItem> list, TItem item)
         {
             List = list;
             Item = item;
         }
 
+        /// <inheritdoc cref="IDisposable.Dispose"/>
         public void Dispose()
         {
             List.Remove(Item);
--- Pipelines/Implementations/Runners/RunningProcessorObservableInformation.cs
+++ Pipelines/Implementations/Runners/RunningProcessorObservableInformation.cs
@@ -1,8 +1,18 @@
 ﻿namespace Pipelines.Implementations.Runners
 {
+    /// <summary>
+    /// Class to be used by observer of processor runner.
+    /// </summary>
     public class RunningProcessorObservableInformation
     {
+        /// <summary>
+        /// Processor that is supposed to be run.
+        /// </summary>
         public IProcessor Processor { get; set; }
+
+        /// <summary>
+        /// Arguments that are used to run the pipeline.
+        /// </summary>
         public object Arguments { get; set; }
     }
 }
