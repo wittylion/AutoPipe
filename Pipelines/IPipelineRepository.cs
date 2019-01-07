9ee879836ecfda945c59dce46f3d0bb2ada11176
Sergey Solomentsev <serg.solomentsev@ya.ru>
2019-01-07 20:16:45 +0200

    Added pipeline repository for future use in extension libraries.

---
+++ Pipelines/IPipelineRepository.cs
@@ -0,0 +1,8 @@
+﻿namespace Pipelines
+{
+    public interface IPipelineRepository
+    {
+        void GetSingle<TQuery>(TQuery query) where TQuery : QueryContext<IPipeline>;
+        void GetMany<TQuery>(TQuery query) where TQuery : QueryContext<IPipeline[]>;
+    }
+}
