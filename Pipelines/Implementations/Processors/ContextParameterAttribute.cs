6adb9146eb1e9c245ffb05b6d151cd22b2079d2b
SergAtGitHub <Serg.Solomentsev@ya.ru>
2020-08-24 13:53:25 +0000

    First view onto the self descriptive processor.

---
+++ Pipelines/Implementations/Processors/ContextParameterAttribute.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Pipelines.Implementations.Processors
+{
+    [AttributeUsage(AttributeTargets.Parameter, AllowMultiple = false, Inherited = false)]
+    public class ContextParameterAttribute : Attribute
+    {
+        public string Name { get; set; }
+        public bool Required { get; set; }
+    }
+}
---
+++ Pipelines/Implementations/Processors/ExecuteMethodAttribute.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Pipelines.Implementations.Processors
+{
+    [AttributeUsage(AttributeTargets.Method, AllowMultiple = false, Inherited = false)]
+    public class ExecuteMethodAttribute : Attribute
+    {
+        public int Order { get; set; }
+    }
+}
