6f9e2a31fee20e2d1428470c4bc7241b953c8181
SergAtGitHub <Serg.Solomentsev@ya.ru>
2020-08-24 13:55:48 +0000

    Fixing using errors.

--- Pipelines/Implementations/Processors/ExplanarotyProcessor.cs
+++ Pipelines/Implementations/Processors/ExplanarotyProcessor.cs
@@ -1,8 +1,8 @@
-﻿using System;
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using System.Reflection;
-using System.Text;
+using System.Threading.Tasks;
 
 namespace Pipelines.Implementations.Processors
 {
